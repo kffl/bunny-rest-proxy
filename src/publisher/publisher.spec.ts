687829d7ec2d2e64303d2c2d6576259ae5542466
Paweł Kuffel <pawel@kuffel.io>
2022-01-02 13:33:25 +0100

    test(publisher): use jest-mock-extended and jest module mocks instead of manual ones

--- src/publisher/publisher.spec.ts
+++ src/publisher/publisher.spec.ts
@@ -4,49 +4,44 @@ import { BinaryMessageParser } from '../message-parser/binary';
 import { FastifyLoggerInstance } from 'fastify';
 import { MessageContentType } from '../message-parser/content-types';
 import { PublisherErrors } from './errors';
+import { mock } from 'jest-mock-extended';
 
 const mockAssertQueue = jest.fn(() => Promise.resolve());
 const mockSendToQueue = jest.fn(() => Promise.resolve());
-const mockSendToQueueFail = jest.fn(() => Promise.reject());
-const mockLogError = jest.fn();
 
-// @ts-ignore
-const mockChannel = {
-    assertQueue: mockAssertQueue,
-    sendToQueue: mockSendToQueue
-} as ConfirmChannel;
-
-// @ts-ignore
-const mockChannelFailing = {
-    assertQueue: mockAssertQueue,
-    sendToQueue: mockSendToQueueFail
-} as ConfirmChannel;
-
-// @ts-ignore
-const mockLogger = {
-    error: mockLogError
-} as FastifyLoggerInstance;
+jest.mock('amqplib-as-promised/lib', () => {
+    return {
+        ConfirmChannel: jest.fn().mockImplementation(() => {
+            return {
+                assertQueue: mockAssertQueue,
+                sendToQueue: mockSendToQueue
+            };
+        })
+    };
+});
 
 describe('Publisher', () => {
     beforeEach(() => {
         mockAssertQueue.mockClear();
         mockSendToQueue.mockClear();
-        mockLogError.mockClear();
     });
 
     it('should assert a durable queue with specified name', async () => {
-        const p = new Publisher('testqueue', mockChannel, new BinaryMessageParser());
+        //@ts-ignore
+        const p = new Publisher('testqueue', new ConfirmChannel(), new BinaryMessageParser());
         await p.assertQueue();
         expect(mockAssertQueue).toHaveBeenCalledTimes(1);
         expect(mockAssertQueue).toHaveBeenCalledWith('testqueue', { durable: true });
     });
 
     it('should return result upon sending a message', async () => {
-        const p = new Publisher('testqueue', mockChannel, new BinaryMessageParser());
+        const mockLogger = mock<FastifyLoggerInstance>();
+        //@ts-ignore
+        const p = new Publisher('testqueue', new ConfirmChannel(), new BinaryMessageParser());
         const response = await p.sendMessage(
             { 'content-type': MessageContentType.binary },
             Buffer.from('testmessage'),
-            {} as FastifyLoggerInstance
+            mockLogger
         );
 
         expect(mockSendToQueue.mock.calls.length).toBe(1);
@@ -60,10 +55,14 @@ describe('Publisher', () => {
         const uuidRegex =
             /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
         expect(response.messageId).toMatch(uuidRegex);
+        expect(mockLogger.error).not.toHaveBeenCalled();
     });
 
     it('should throw an error if message sending fails', async () => {
-        const p = new Publisher('testqueue', mockChannelFailing, new BinaryMessageParser());
+        const mockLogger = mock<FastifyLoggerInstance>();
+        mockSendToQueue.mockRejectedValueOnce('some error');
+        //@ts-ignore
+        const p = new Publisher('testqueue', new ConfirmChannel(), new BinaryMessageParser());
         const t = async () => {
             await p.sendMessage(
                 { 'content-type': MessageContentType.binary },
@@ -73,6 +72,6 @@ describe('Publisher', () => {
         };
 
         await expect(t()).rejects.toThrow(PublisherErrors.ERR_QUEUE_DOWN);
-        expect(mockLogError).toHaveBeenCalledTimes(1);
+        expect(mockLogger.error).toHaveBeenCalledTimes(1);
     });
 });
